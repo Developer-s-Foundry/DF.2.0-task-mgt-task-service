74d9cfbe6aac1292ddffb1660139e81bfd70541f
oladapo <oladapoelijah49@gmail.com>
2025-09-16 21:29:48 +0100

    made changes on the database setup

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,21 +1,21 @@
 import { DataSource } from 'typeorm';
 import { config } from './config/config';
 
-const setupDatabase = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: `${config.DATABASE_USER}`,
   password: `${config.DATABASE_PASSWORD}`,
   database: `${config.DATABASE_NAME}`,
-  entities: ['src/entity/*.ts'],
+  entities: ['src/entities/*{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
   logging: true,
-  synchronize: true,
 });
 
 export const databaseConnection = async () => {
   try {
-    await setupDatabase.initialize();
+    await AppDataSource.initialize();
     console.log('Data Source has been initialized!');
   } catch (error) {
     console.error('Error during Data Source initialization:', error);
